21341bdafbd2c2b0860bd3fa7074a723e3828301
Nicholas English <englisni@arch07.cis.gvsu.edu>
2018-03-20 13:21:00 -0400

    Finished file.

--- include/superhero.h
+++ include/superhero.h
@@ -7,8 +7,39 @@
  * @date 3/21/2018
  *********************************************************************/
 
+#ifndef __H_superhero__
+#define __H_superhero__
+
+
 #include <string>
 #include <iostream>
 
 boolean insert(const Superhero & s);
 Superhero & get(const std::string name);
+
+class superhero {
+
+    public:
+	superhero(int page_id, std::string name, std::string urlslug,
+	    std::string id, sttd::string alignment, char eye_color,
+	    char hair_color, char sex, std::string gsm, bool alive,
+	    int appearences, std::string first_appearance, int year);
+
+    private:
+	int page_id;
+	std::string name;
+	std::string urlslug;
+	std::string id;
+	std::string alignment;
+	char eye_color;
+	char hair_color;
+	char sex;
+	std::string gsm;
+	bool alive;
+	int appearances;
+	std::string first_appearance;
+	int year;
+
+};
+
+#endif // __H_superhero__
